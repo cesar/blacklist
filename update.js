65d089e9a61ef0619ff1e6b3cd936a83b27b5e1f
sezalcruz <cesarcruz91@gmail.com>
2016-08-14 20:23:41 -0400

    inserting IP, need to handle cases in which there is a netset and cases in which using CIDR notation

--- update.js
+++ update.js
@@ -1,10 +1,15 @@
 'use strict'
 const vfs = require('vinyl-fs')
 const redis = require('redis')
-// const client = redis.createClient()
+const client = redis.createClient()
 const Git = require('nodegit')
 const path = require('path')
 const fs = require('fs')
+
+// Redis Keys
+const OLD_SET = 'old:ipsets:set'
+const NEW_SET = 'new:ipsets:set'
+const CURRENT_SET = 'current:ipsets:set'
 
 
 // var fileStream = vfs.src(['./list/blocklist-ipsets/**/*.ipset', './list/blocklist-ipsets/**/*.netset'], {
@@ -101,19 +106,78 @@ function _pullRepo (dir) {
     })
 }
 
-refreshIpsets().then(() => {
-  console.log('repo updated')
-}).catch(err => {
-  console.log(err)
-})
+/**
+ * Remove the previous ipset list
+ */
+function removeOldIpsets () {
+  return new Promise((resolve, reject) => {
+    client.del(OLD_SET, (err, reply) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(reply)
+    })
+  })
+}
 
-// function removeOldIpsets () {
-//   return new Promise
-// }
+/**
+ * Fetch all of the files in the ipset-blocklist
+ * and add the ipsets to redis
+ */
+function addNewIpsets () {
+  return new Promise((resolve, reject) => {
+    // Fetch the new list of items
+    let fileStream = vfs.src(['./list/**/*.ipset', './list/**/*.netset'], {
+      buffer: false
+    })
 
-// function addNewIpsets () {
+    fileStream.on('data', function (file) {
+      fileStream.pause()
+      file.contents.setEncoding('utf8')
+      file.contents.on('data', function (data) {
+        file.contents.pause()
+        // Fetch the actual IPs
+        let ips
+        if (Buffer.isBuffer(data)) {
+          ips = data.toString('utf8').split('\n').filter(function (item) {
+            return !item.startsWith('#') && item.trim()
+          })
+          file.contents.resume()
+        } else {
+          ips = data.split('\n').filter(function (item) {
+            return !item.startsWith('#') && item.trim()
+          })
+          if (ips.length > 0) {
+            client.sadd(NEW_SET, ips, function (err, replies) {
+              if (err) {
+                reject(err)
+              }
+              file.contents.resume()
+            })
+          } else {
+            file.contents.resume()
+          }
+        }
+      })
 
-// }
+      file.contents.on('error', (err) => {
+        reject(err)
+      })
+
+      file.contents.on('end', function () {
+        fileStream.resume()
+      })
+    })
+
+    fileStream.on('error', err => {
+      reject(err)
+    })
+
+    fileStream.on('end', () => {
+      resolve()
+    })
+  })
+}
 
 // function swapIpsets () {
 
@@ -122,4 +186,12 @@ refreshIpsets().then(() => {
 // function main () {
 
 // }
+
+addNewIpsets().then(result => {
+  console.log('Success')
+  client.quit()
+}).catch(err => {
+  console.log(err)
+  client.quit()
+})
 
