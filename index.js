b076997171639ac90aa29f6dfbcefdbc1578184e
sezalcruz <cesarcruz91@gmail.com>
2016-08-17 02:28:53 -0400

    Added restart mechanism, round-robin fashion

--- index.js
+++ index.js
@@ -3,42 +3,43 @@
 //  TODO: Configure startup script using `npm start`
 //  TODO: Add flag to indicate which lists are loaded (all or base case)
 //  TODO: Create a README.md
-//  TODO: Move from Express to HTTP
 //  TODO: Test whether error on master tries to fork more processes
 //  TODO: Switch from Vinyl to Glob module for looking at files
 //  TODO: Automated tests
 //  TODO: Configure Prod
 //  TODO: Handle case where a worker gets killed
 
-const express = require('express')
-const app = express()
-const multer = require('multer')
-const upload = multer()
 const list = require('./lib/list')
 const path = require('path')
 const cluster = require('cluster')
 
 let listPath = path.join(__dirname, 'blocklist-ipsets')
-const REFRESH_PERIOD = 1000 * 60 * 2
+
+const REFRESH_PERIOD = 120000
 
 if (cluster.isMaster) {
-  // Bootstrap - clone or pull the repo
-  list.fetch(listPath)
-  // Start workers and listen for messages containing notifyRequest
-  const cores = require('os').cpus().length
-  for (var i = 0; i < cores; i++) {
-    cluster.fork()
-  }
-  // Update the repo and notify the processes
-  setInterval(function () {
-    list.fetch(listPath)
-    for (let id in cluster.workers) {
-      // Don't refresh them all at once
-      setTimeout(() => {
-        cluster.workers[id].send('refresh')
-      }, 10000)
+  let workerQueue = []
+
+  list.fetch(listPath).then(() => {
+    const cores = require('os').cpus().length
+    for (var i = 0; i < cores; i++) {
+      cluster.fork()
     }
-  }, REFRESH_PERIOD) // Every 12 mins
+    // Update the repo and notify the processes
+    setInterval(function () {
+      console.log('Time for a refresh')
+      list.fetch(listPath).then(() => {
+        for (let id in cluster.workers) {
+          workerQueue.push(cluster.workers[id]) // Fill the queu of workers
+        }
+        workerQueue.pop().send('refresh') // Start the refresh process
+      })
+    }, REFRESH_PERIOD) // Every 12 mins
+
+  }).catch(err => {
+    console.error(err)
+    process.exit(0)
+  })
 
   //Restart a worker that has failed
   cluster.on('exit', (worker, code, signal) => {
@@ -46,43 +47,65 @@ if (cluster.isMaster) {
     cluster.fork()
   })
 
+  // Worker just refreshed a list, on to the next one
+  cluster.on('message', (message) => {
+    if (workerQueue.length > 0) workerQueue.pop().send('refresh')
+    console.log(message)
+  })
+
 } else {
+
+  const http = require("http")
   let server
   let blocklist
+
   // Bootstrap
-  list.load(function (err, list) {
-    if (err) {
-      console.log(err)
-    }
-    blocklist = list
-    server = app.listen(3000, function () {
-      console.log('Server up on worker: ' + cluster.worker.id)
+  list.load(listPath).then(result => {
+    blocklist = result
+    server = http.createServer(requestHandler)
+    server.listen(3000, () => {
+      console.log("Server is listening")
+      process.send('process-started')
     })
+  }).catch(err => {
+    console.error(err)
+    process.exit(0)
   })
 
-  app.post('/check', upload.array(), function (req, res) {
-    // TODO: check whether address is an actual IP (needed?)
-    let result = blocklist.contains(req.body.address.trim())
-    res.json({
-      contains: result
-    })
-  })
-
-  // refresh and swap
+  // refresh
   process.on('message', (msg) => {
     if (msg === 'refresh') {
-      console.log('refreshing')
       // TODO: Stop accepting requests until the list is update
-      server.close(function(){
-        list.load((err, list) => {
-          if (err) {
-            // TODO: Handle Error
-            console.log(err)
-          }
-          blocklist = list
-          server.listen(3000)
-        })
+      server.close(() => {
+        // Connections closed
+        console.log('Connection closed, exiting')
+        process.exit(0)
       })
     }
   })
+
+  function requestHandler (request, response) {
+    let data = []
+    let flag = false
+    request.on('data', function(chunk) {
+      data.push(chunk);
+    }).on('end', function() {
+      data = Buffer.concat(data).toString();
+      let match = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/.exec(data)
+      console.log(match)
+      if (match) {
+        data = match[0].trim() //Extract the ip
+        // TODO: Perform check here
+        console.log(data)
+        flag = blocklist.contains(data)
+        console.log(flag)
+        if (flag) {
+          response.writeHead(200, {"Content-Type": "text/html"});
+        } else {
+          response.writeHead(404, {"Content-Type": "text/html"});
+        }
+        response.end();
+      }
+    });
+  }
 }
